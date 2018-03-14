10833042fb447c4a113a00e48f65878268b3b3df
kevin lima <kevin.lima.code@gmail.com>
2018-03-14 22:25:21 +0000

    I added an Ajax and Api from the PopCode Exercise to use to make the foundtaion of my GiphyFinder

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-$(document).ready(function(){
-  
-  
-  
-  
+$(document).ready(function() {
+    $.ajax({
+        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=13",
+        method: "GET",
+        success: function(response) {
+            //Log the orignal image to the console    
+            var image = response.data[5].images.original.url;
+            //Log the url from the orignial gif to the console  
+            $('body').append('<img src=' + image + '>');
+        },
+    });
+
+
+
 });
