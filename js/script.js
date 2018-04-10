3099b8cd8616ab7a318e560b36e187fabeb589c5
kevin lima <kevin.lima.code@gmail.com>
2018-04-10 20:48:43 +0000

    I added an Id for the click and added a click function and variable so that the input wuld be saved and later called for the api

--- js/script.js
+++ js/script.js
@@ -3,17 +3,25 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function() {
-    $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=13",
-        method: "GET",
-        success: function(response) {
-            //Log the orignal image to the console    
-            var image = response.data[5].images.original.url;
-            //Log the url from the orignial gif to the console  
-            $('body').append('<img src=' + image + '>');
-        },
+    console.log('the document is ready');
+    function getGiphy(i) {
+        console.log('The getGiphy Function was called');
+        $.ajax({
+            url: "https://api.giphy.com/v1/stickers/search?q=" + i + "&api_key=dc6zaTOxFJmzC&limit=13",
+            method: "GET",
+            success: function(response) {
+                //Log the orignal image to the console    
+                var image = response.data[5].images.original.url;
+                //Log the url from the orignial gif to the console  
+                $('body').append('<img src=' + image + '>');
+            },
+        });
+    }
+    $("#search").click(function() {
+        console.log('the button was clicked');
+        var input = $("#search-term").val();
+        console.log(input);
+        getGiphy(input);
     });
-
-
 
 });
